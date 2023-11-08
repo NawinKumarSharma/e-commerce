6a3acbf2d81cdeaf9517415aaeb246061ff4b1d9
NawinKumarSharma <nawinsharma60@gmail.com>
2023-11-08 23:25:33 +0530

    :sparkles: adda all-new-products page

--- app/all-new-products/page.tsx
+++ app/all-new-products/page.tsx
@@ -1,41 +1,36 @@
-import Image from "next/image";
+import Link from "next/link";
 import { simplifiedProduct } from "../interface";
 import { client } from "../lib/sanity";
-import Link from "next/link";
+import { ArrowRight } from "lucide-react";
+import Image from "next/image";
 
-async function getData(category: string) {
-  const query = `*[_type=="product" && category->name == "${category}"]{
-    _id,
-    "imageUrl":images[0].asset->url,
-    price,
-    name,
-    "slug": slug.current,
-    "categoryName": category->name
-  }`;
+async function getData() {
+  const query = `*[_type == "product"]| order(_createdAt desc) {
+        _id,
+          price,
+        name,
+          "slug": slug.current,
+          "categoryName": category->name,
+          "imageUrl": images[0].asset->url
+      }`;
 
   const data = await client.fetch(query);
-  return data;
 
+  return data;
 }
 
-export const dynamic = "force-dynamic";
-
-export default async function CategoryPage({
-  params,
-}: {
-  params: { category: string };
-}) {
-  const data: simplifiedProduct[] = await getData(params.category)
+export default async function All_New_Products() {
+  const data: simplifiedProduct[] = await getData();
 
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-            Our Products for {params.category}
+            All Our Newest products
           </h2>
-
         </div>
+
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {data.map((product) => (
             <div key={product._id} className="group relative">
@@ -66,6 +61,7 @@ export default async function CategoryPage({
                       ${product.price}
                     </p>
                   </div>
+
                 </div>
               </div>
             </div>
