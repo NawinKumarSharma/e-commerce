38c16cdaf4e131bc29cce450d5c63c59952b526e
NawinKumarSharma <nawinsharma60@gmail.com>
2023-11-08 22:50:04 +0530

    :sparkles: Update 'Our newest product' section

--- app/components/Newest.tsx
+++ app/components/Newest.tsx
@@ -42,13 +42,15 @@ export default async function Newest() {
           {data.map((product) => (
             <div key={product._id} className="group relative">
               <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
-                <Image
-                  src={product.imageUrl}
-                  alt="Product image"
-                  className="w-full h-full object-cover object-center lg:h-full lg:w-full"
-                  width={300}
-                  height={300}
-                />
+                <Link href={`/product/${product.slug}`}>
+                  <Image
+                    src={product.imageUrl}
+                    alt="Product image"
+                    className="w-full h-full object-cover object-center lg:h-full lg:w-full"
+                    width={300}
+                    height={300}
+                  />
+                </Link>
               </div>
 
               <div className="mt-4 flex justify-between">
@@ -58,13 +60,16 @@ export default async function Newest() {
                       {product.name}
                     </Link>
                   </h3>
-                  <p className="mt-1 text-sm text-gray-500">
-                    {product.categoryName}
-                  </p>
+                  <div className="flex items-center space-x-5">
+                    <p className="text-sm text-gray-500">
+                      {product.categoryName}
+                    </p>
+                    <p className="text-md font-extralight text-blue-600">
+                      ${product.price}
+                    </p>
+                  </div>
+
                 </div>
-                <p className="text-sm font-medium text-gray-900">
-                  ${product.price}
-                </p>
               </div>
             </div>
           ))}
